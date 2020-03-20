5e24cb54da6e26f3ee29b3953b93efd426c2ba45
Adam Lancaster <lancaster28@hotmail.com>
2020-03-20 13:37:43 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GcpSecretProvider.MixProject do
   def project do
     [
       app: :gcp_secret_provider,
-      version: "0.1.0",
+      version: "0.1.1",
       description: description(),
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
